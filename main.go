96aebcdb232a76f8d8fe78bee1d4c2c881f3244b
Jan S <7893682+janstuemmel@users.noreply.github.com>
2026-02-08 20:18:42 +0100

    add read from stdin

--- main.go
+++ main.go
@@ -27,9 +27,50 @@ type Message struct {
 	Content string `json:"content"`
 }
 
-func main() {
-	fmt.Println(term.IsTerminal(int(os.Stdin.Fd())))
+func chatLoop(client *internal.OpenRouterClient) {
+	messages := []Message{}
+	reader := bufio.NewReader(os.Stdin)
 
+	for {
+		fmt.Fprint(os.Stdout, "> ")
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		prompt := strings.TrimSpace(line)
+
+		if prompt == "" {
+			continue
+		}
+
+		answer := ""
+		messages = append(messages, Message{Role: "user", Content: prompt})
+		payload := map[string]any{
+			"model":    "openai/gpt-5",
+			"stream":   true,
+			"messages": messages,
+		}
+
+		client.HandleRequest(payload, func(chunk string) {
+			s, _, _ := internal.HandleOpenRouterChunk(chunk)
+			fmt.Print(s)
+			answer += s
+		})
+
+		messages = append(messages, Message{Role: "assistant", Content: answer})
+		fmt.Println()
+		fmt.Println()
+	}
+}
+
+func main() {
+	isTty := term.IsTerminal(int(os.Stdin.Fd()))
 	ctx, stop := signal.NotifyContext(
 		context.Background(),
 		os.Interrupt,
@@ -46,45 +87,32 @@ func main() {
 	client := internal.NewOpenRouterClient(ctx, apiKey)
 
 	go func() {
-		messages := []Message{}
-		reader := bufio.NewReader(os.Stdin)
-
-		for {
-			fmt.Fprint(os.Stdout, "> ")
-			line, err := reader.ReadString('\n')
+		if isTty {
+			chatLoop(client)
+		} else {
+			in, err := io.ReadAll(os.Stdin)
 
 			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
+				return
 			}
 
-			prompt := strings.TrimSpace(line)
-
-			if prompt == "" {
-				continue
-			}
-
-			answer := ""
-			messages = append(messages, Message{Role: "user", Content: prompt})
+			prompt := strings.TrimSpace(string(in))
 			payload := map[string]any{
 				"model":    "openai/gpt-5",
 				"stream":   true,
-				"messages": messages,
+				"messages": []Message{{Role: "user", Content: prompt}},
 			}
 
-			err = client.HandleRequest(payload, func(chunk string) {
+			client.HandleRequest(payload, func(chunk string) {
 				s, _, _ := internal.HandleOpenRouterChunk(chunk)
 				fmt.Print(s)
-				answer += s
 			})
 
-			messages = append(messages, Message{Role: "assistant", Content: answer})
-			fmt.Println()
 			fmt.Println()
 		}
+
+		// exit routine
+		stop()
 	}()
 
 	<-ctx.Done()
