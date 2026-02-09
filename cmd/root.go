5e78d48493c08693222c610025430a409fe58a11
Jan S <7893682+janstuemmel@users.noreply.github.com>
2026-02-09 15:02:38 +0100

    add models autocompletion

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,18 @@
 package cmd
 
 import (
+	_ "embed"
+	"encoding/json"
 	"os"
+	"slices"
 
 	"github.com/janstuemmel/oli/internal"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+//go:embed models.json
+var modelsFile string
 
 var (
 	config  internal.Config
@@ -17,11 +23,42 @@ var (
 	}
 )
 
+type Model struct {
+	Id           string `json:"id"`
+	Name         string `json:"name"`
+	Architecture struct {
+		InputModalities  []string `json:"input_modalities"`
+		OutputModalities []string `json:"output_modalities"`
+	} `json:"architecture"`
+	Pricing struct {
+		Prompt            string `json:"prompt"`
+		Completion        string `json:"completion"`
+		Request           string `json:"request"`
+		Image             string `json:"image"`
+		WebSearch         string `json:"web_search"`
+		InternalReasoning string `json:"internal_reasoning"`
+	} `json:"pricing"`
+}
+
+type modelJson struct {
+	Data []Model `json:"data"`
+}
+
 func Execute() error {
 	return rootCmd.Execute()
 }
 
 func init() {
+	var modelJson modelJson
+	var models []string
+	err := json.Unmarshal([]byte(modelsFile), &modelJson)
+	cobra.CheckErr(err)
+	for _, model := range modelJson.Data {
+		if slices.Contains(model.Architecture.InputModalities, "text") && slices.Contains(model.Architecture.OutputModalities, "text") {
+			models = append(models, model.Id)
+		}
+	}
+
 	cobra.OnInitialize(initConfig)
 
 	// online
@@ -42,14 +79,37 @@ func init() {
 	// model
 	rootCmd.PersistentFlags().StringP("model", "m", "", "modle name")
 	viper.BindPFlag("model", rootCmd.PersistentFlags().Lookup("model"))
-	viper.SetDefault("model", "google/gemini-2.5-flash")
+	viper.SetDefault("model", "openrouter/free")
+
 	viper.BindEnv("model", "OLI_MODEL")
+	rootCmd.RegisterFlagCompletionFunc("model", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return models, cobra.ShellCompDirectiveNoFileComp
+	})
 
 	// system prompt
 	rootCmd.PersistentFlags().String("system", "", "system prompt")
 	viper.BindPFlag("system", rootCmd.PersistentFlags().Lookup("system"))
 	viper.SetDefault("system", "")
 	viper.BindEnv("system", "OLI_SYSTEM")
+
+	// completions
+	rootCmd.AddCommand(&cobra.Command{
+		Use:       "completion [bash|zsh|fish|powershell]",
+		Short:     "Generate shell completion script",
+		ValidArgs: []string{"bash", "zsh", "fish", "powershell"},
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				_ = cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				_ = cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				_ = cmd.Root().GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				_ = cmd.Root().GenPowerShellCompletion(os.Stdout)
+			}
+		},
+	})
 }
 
 func initConfig() {
